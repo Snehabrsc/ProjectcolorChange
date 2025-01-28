cc9272cfa57854e66395711d9a3299d051d779da
Snehasree <snehabrsc@gmail.com>
2025-01-28 18:38:47 +0000

    backgrnd theme clr change

--- colorchanger/colorchanger.js
+++ colorchanger/colorchanger.js
@@ -1,9 +1,13 @@
 const buttons=document.querySelectorAll('.button')
-const body=document.querySelector("body")
+const body=document.querySelector('body')
+
+
 
 buttons.forEach( (button) =>{
     button.addEventListener('click',function(e){
-        console.log(e)
-        console.log(e.target)
+       
+            body.style.backgroundColor=e.target.id
+      
     })
 })
+
